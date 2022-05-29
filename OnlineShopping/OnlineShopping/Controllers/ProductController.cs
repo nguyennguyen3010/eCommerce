563b262199b07eb9064121ce36714536655fb740
PhamQuocDuy <phamquocduy1110@gmail.com>
2022-05-29 19:26:48 +0700

    -Load data from data to

--- OnlineShopping/OnlineShopping/Controllers/ProductController.cs
+++ OnlineShopping/OnlineShopping/Controllers/ProductController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlineShopping.Models;
 
 namespace OnlineShopping.Controllers
 {
     public class ProductController : Controller
     {
+        ShoppingEntities model = new ShoppingEntities();
+
         // GET: Product
         public ActionResult Index()
         {
-            return View();
+            var maxquestions = model.HangHoas.OrderBy(p => p.MaHH).Take(12).ToList();
+            return View(maxquestions);
         }
         public ActionResult Details()
         {
