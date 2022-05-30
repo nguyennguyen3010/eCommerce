2239a7f53802dc8bf02808d64992caac294ecc70
PhamQuocDuy <phamquocduy1110@gmail.com>
2022-05-30 12:21:48 +0700

    - Add database to project

---
+++ WGShopping/WGShopping/Data/Migrations/20220530051551_InitDb01.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WGShopping.Data.Migrations
+{
+    public partial class InitDb01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ WGShopping/WGShopping/Entities/HoaDon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WGShopping.Entities
+{
+    public partial class HoaDon
+    {
+        public HoaDon()
+        {
+            ChiTietHds = new HashSet<ChiTietHd>();
+        }
+
+        public int MaHd { get; set; }
+        public string MaKh { get; set; } = null!;
+        public DateTime NgayDat { get; set; }
+        public DateTime NgayCan { get; set; }
+        public DateTime NgayGiao { get; set; }
+        public string? HoTen { get; set; }
+        public string DiaChi { get; set; } = null!;
+        public string CachThanhToan { get; set; } = null!;
+        public string CachVanChuyen { get; set; } = null!;
+        public double PhiVanChuyen { get; set; }
+        public int TrangThai { get; set; }
+        public string? MaVn { get; set; }
+        public string? GhiChu { get; set; }
+
+        public virtual KhachHang MaKhNavigation { get; set; } = null!;
+        public virtual NhanVien? MaVnNavigation { get; set; }
+        public virtual ICollection<ChiTietHd> ChiTietHds { get; set; }
+    }
+}
